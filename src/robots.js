820eccfbbc9584cd57c12bfe832866c6b4d10aa2
Jonas Coppens <j.coppens@televic.com>
2020-06-12 13:09:17 +0200

    initial commit for robofriends

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Jonas Coppens',
+        username: 'jcop',
+        email: 'joc@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'Veroniek Van Driessche',
+        username: 'vvdries',
+        email: 'vvd@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'Abel Coppens',
+        username: 'ananas',
+        email: 'ac@gmail.com'
+    },
+    {
+        id: 4,
+        name: 'Noah Coppens',
+        username: 'nootje',
+        email: 'nc@gmail.com'
+    },
+    {
+        id: 5,
+        name: 'Jo Coppens',
+        username: 'jopa',
+        email: 'jc@gmail.com'
+    },
+    {
+        id: 6,
+        name: 'Kristina De Cooman',
+        username: 'nona',
+        email: 'kdc@gmail.com'
+    },
+    {
+        id: 7,
+        name: 'Bram Coppens',
+        username: 'xz32',
+        email: 'bc@gmail.com'
+    },
+    {
+        id: 8,
+        name: 'Sam Coppens',
+        username: 'herm',
+        email: 'sc@gmail.com'
+    },
+    {
+        id: 9,
+        name: 'Celine Coeckelberghs',
+        username: 'celi',
+        email: 'cc@gmail.com'
+    },
+    {
+        id: 10,
+        name: 'Annelien Van Driessche',
+        username: 'annelien',
+        email: 'avd@gmail.com'
+    },
+]
